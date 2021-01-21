2d5628875959ffa5034a128ded5ba5ca328af7ae
sharsha2 <karuturiharsh@gmail.com>
2021-01-21 02:04:51 -0500

    Created drop down directives in shared folder which added a dropdown list key feature to th e nav and th e Recipe detial list dropdown

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  // tslint:disable-next-line:typedef
+  @HostListener('click') toggleOpen(){
+    this.isOpen = !this.isOpen;
+  }
+}
+
